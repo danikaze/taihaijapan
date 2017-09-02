3e9025f6ad684c9090d33c81094974ecd0280034
danikaze <danikaze@gmail.com>
2017-09-02 15:32:34 +0900

    Add photoswipe in the gallery page

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -6,13 +6,18 @@ import googleAnalytics from './util/GoogleAnalytics';
 import ListGallery from './util/ListGallery';
 
 const THUMBNAIL_ID = 'thumbnails';
+const GALLERY_ID = 'gallery';
 
 function run() {
   requestJson(settings.galleryDbUrl, settings.ajax)
     .then((data) => {
       const galleryContainer = document.getElementById(THUMBNAIL_ID);
+      const galleryViewer = document.getElementById(GALLERY_ID);
+
       // eslint-disable-next-line no-new
-      new ListGallery(galleryContainer, data.photos);
+      new ListGallery(galleryContainer, galleryViewer, data.photos, {
+        sizes: data.sizes,
+      });
     });
 
   googleAnalytics.insert();
