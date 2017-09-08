edcc910b041752370771933f179cf0e36dd4dd9c
danikaze <danikaze@gmail.com>
2017-09-09 04:08:09 +0900

    Fix polyfill for Array.findIndex =_=

--- src/js/polyfills/Array.findIndex.js
+++ src/js/polyfills/Array.findIndex.js
@@ -3,12 +3,12 @@
 if (!Array.prototype.findIndex) {
   /**
    * Same as Array.filter but returning the index of the first found element
-   * @param {Array<*>}      array      Array to search within
+   * @this {Array<*>} Array to search within
    * @param {(any):boolean} callback   Function returning `true` if it's the desired element
    */
-  Array.prototype.findIndex = (array, callback) => {
-    for (let i = 0; i < array.length; i++) {
-      if (callback(array[i])) {
+  Array.prototype.findIndex = (callback) => {
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i])) {
         return i;
       }
     }
