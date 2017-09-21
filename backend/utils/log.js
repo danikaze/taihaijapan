155ce35492b29070ed96ec52cb2c31143f5120f3
danikaze <danikaze@gmail.com>
2017-09-21 21:50:31 +0900

    Add date to logs (configurable)

--- backend/utils/log.js
+++ backend/utils/log.js
@@ -1,6 +1,18 @@
 const npmlog = require('npmlog');
 const settings = require('../settings');
 
+function getDateString() {
+  const d = new Date();
+  return `${d.getFullYear()}-`
+    + `${String(d.getMonth() + 1).padStart(2, '0')}-`
+    + `${String(d.getDate()).padStart(2, '0')} `
+    + `${String(d.getHours()).padStart(2, '0')}:`
+    + `${String(d.getMinutes()).padStart(2, '0')}:`
+    + `${String(d.getSeconds()).padStart(2, '0')}.`
+    + `${String(d.getMilliseconds()).padStart(2, '0')}`
+    ;
+}
+
 /*
  * log levels:
  *  silly
@@ -11,5 +23,11 @@ const settings = require('../settings');
  *  error
  */
 npmlog.level = settings.server.logLevel;
+
+if (settings.server.logDate) {
+  npmlog.on('log', (data) => {
+    data.message = `[${getDateString()}] ${data.message}`;
+  });
+}
 
 module.exports = npmlog;
