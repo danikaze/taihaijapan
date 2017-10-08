a980efcd0a4b227ecb21fd0f00517f24113c6ca7
danikaze <danikaze@gmail.com>
2017-10-08 23:51:53 +0900

    Readded fit images on index page load/resize

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,41 +1,154 @@
-// import 'es6-object-assign/auto';
-// import 'es6-promise/auto';
-// import './polyfills/Array.findIndex';
+import fitRects from './util/fitRects';
+import SrcSetEmu from './util/SrcSetEmu';
 
-// import '../styles/index.scss';
+const thumbnails = document.getElementById('thumbnails');
+const options = {
+  scrollFrequency: 1000 / 60,
+  scrollDuration: 150,
+  marginH: 10,
+};
 
-// import dailyBg from './util/dailyBg';
-// import requestJson from './util/requestData/requestJson';
-// import settings from './util/settings';
-// import VerticalGallery from './util/VerticalGallery';
-// import googleAnalytics from './util/GoogleAnalytics';
-// import addEventListener from './util/addEventListener';
+let photoList;
+let animatedScrollTimeout;
+let srcSetEmu;
 
-// const BG_ID = 'bgImg';
-// const THUMBNAIL_ID = 'thumbnails';
-// const SOCIAL_ID = 'social';
-// const VIEW_MORE_ID = 'viewMore';
+/**
+ * Scroll the window an offset in the specified time
+ */
+function animateScroll(offsetX, offsetY, time) {
+  if (animatedScrollTimeout) {
+    return;
+  }
 
-// // setMockupData(settings.galleryDbUrl, galleryMockupData);
+  let steps = (time || options.scrollDuration) / options.scrollFrequency;
+  const dX = offsetX / steps;
+  const dY = offsetY / steps;
+  const targetX = window.scrollX - offsetX;
+  const targetY = window.scrollY - offsetY;
 
-// function run() {
-//   requestJson(settings.galleryDbUrl, settings.ajax)
-//     .then((data) => {
-//       const background = document.getElementById(BG_ID);
-//       const galleryContainer = document.getElementById(THUMBNAIL_ID);
-//       dailyBg.set(background, data.photos, data.sizes);
-//       settings.gallery.topDeadElement = document.getElementById(SOCIAL_ID);
-//       settings.gallery.bottomDeadElement = document.getElementById(VIEW_MORE_ID);
-//       const galleryOptions = Object.assign({}, settings.index, { sizes: data.sizes });
-//       // eslint-disable-next-line no-new
-//       new VerticalGallery(galleryContainer, data.photos, galleryOptions);
+  function move() {
+    steps--;
+    clearTimeout(animatedScrollTimeout);
 
-//       addEventListener(window, 'resize', () => {
-//         dailyBg.set(background, data.photos, data.sizes);
-//       });
-//     });
+    if (steps < 1) {
+      window.scroll(targetX, targetY);
+      animatedScrollTimeout = null;
+    } else {
+      window.scroll(window.scrollX - dX, window.scrollY - dY);
+      animatedScrollTimeout = setTimeout(move, options.scrollFrequency);
+    }
+  }
 
-//   googleAnalytics.insert();
-// }
+  move();
+}
 
-// run();
+/**
+ * Center a bounding client rect in the screen
+ */
+function centerBounds(bounds, time) {
+  if (!bounds) {
+    return;
+  }
+
+  const dX = (window.innerWidth / 2) - bounds.left - (bounds.width / 2);
+  const dY = (window.innerHeight / 2) - bounds.top - (bounds.height / 2);
+  animateScroll(dX, dY, time);
+}
+
+/**
+ * Center a photo in the screen after a scroll
+ */
+function centerImage(targetImg, time, force) {
+  if (!targetImg) {
+    targetImg = getMostCenteredImage();
+  }
+
+  if (!targetImg) {
+    return;
+  }
+
+  centerBounds(targetImg.getBoundingClientRect(), time);
+}
+
+/**
+ * Get the bounds of the image that needs to be centered
+ * If the scroll is too high, it assumes no image is centered,
+ *
+ * @returns {<HTMLImageElement>} Most centered `<img>` element or `null` if none
+ */
+function getMostCenteredImage() {
+  const screenCenterX = window.innerWidth / 2;
+  const screenCenterY = window.innerHeight / 2;
+  let minDistance = Infinity;
+  let targetImage;
+  let bounds;
+  let dist;
+
+  for (let i = 0; i < photoList.length; i++) {
+    const img = photoList[i].elem;
+    bounds = img.getBoundingClientRect();
+    dist = Math.abs(((bounds.left + bounds.width) / 2) - screenCenterX)
+         + Math.abs(((bounds.top + bounds.bottom) / 2) - screenCenterY);
+
+    if (dist < minDistance) {
+      targetImage = img;
+      minDistance = dist;
+    } else {
+      return targetImage;
+    }
+  }
+
+  return targetImage;
+}
+
+/**
+ * Resize an image to fit in a box
+ */
+function fitImage(photo, maxW, maxH) {
+  if (!maxW) {
+    photo.elem.onload = null;
+    maxW = thumbnails.offsetWidth;
+    maxH = window.innerHeight - (options.marginH * 2);
+  }
+  const elem = photo.elem;
+  const size = fitRects(elem.width, elem.height, maxW, maxH);
+  elem.style.width = `${size.w}px`;
+
+  srcSetEmu.updateImage(photo.id);
+}
+
+/**
+ * Resize photos to fit in the screen
+ */
+function fitImages(center) {
+  const maxW = thumbnails.offsetWidth;
+  const maxH = window.innerHeight - (options.marginH * 2);
+  const currentImg = getMostCenteredImage();
+
+  photoList.forEach((img) => fitImage(img, maxW, maxH));
+  if (center !== false) {
+    centerImage(currentImg);
+  }
+}
+
+function start(sizes, photos) {
+  const imgElems = thumbnails.querySelectorAll('img');
+  srcSetEmu = new SrcSetEmu(sizes, null, { auto: false });
+
+  photos.forEach((photo, index) => {
+    const img = imgElems[index];
+    srcSetEmu.addImage(img, photo.imgs, photo.id);
+    photo.elem = img;
+
+    if (img.with && img.height) {
+      fitImage(photo);
+    } else {
+      img.onload = fitImage.bind(null, photo, null, null);
+    }
+  });
+
+  photoList = photos;
+  window.addEventListener('resize', fitImages);
+}
+
+window.start = start;
