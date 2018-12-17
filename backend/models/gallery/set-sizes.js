ee413a90deeb9d6d5bc7d9682c7fce2f34b2aaf6
danikaze <danikaze@gmail.com>
2018-12-17 11:26:04 +0900

    Optimization: Update only the changed sizes

--- backend/models/gallery/set-sizes.js
+++ backend/models/gallery/set-sizes.js
@@ -70,6 +70,18 @@ function removeSize(sizeId) {
 }
 
 /**
+ * Compare two sizes and check if they are the same by value
+ *
+ * @param {*} a
+ * @param {*} b
+ */
+function isEqualSize(a, b) {
+  return a.label === b.label
+    && a.width === b.width
+    && a.height === b.height;
+}
+
+/**
  * Set the thumbnail sizes, updating, removing and adding the needed ones
  *
  * @param {*} sizes Array<{ id, label, width, height }>
@@ -83,8 +95,15 @@ function setSizes(newSizes) {
     const toDelete = currentSizes.filter(toBeDeleted);
     const promises = toDelete.map((size) => removeSize(size.id));
 
-    newSizes.forEach((size) => {
-      promises.push(size.id ? updateSize(size) : addSize(size));
+    newSizes.forEach((newSize) => {
+      if (newSize.id) {
+        const size = currentSizes[currentSizes.findIndex((currentSize) => currentSize.id === newSize.id)];
+        if (!isEqualSize(size, newSize)) {
+          promises.push(updateSize(newSize));
+        }
+      } else {
+        promises.push(addSize(newSize));
+      }
     });
 
     Promise.all(promises)
