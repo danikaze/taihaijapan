be2785454668421bce05075540f87fc4e95bf85a
danikaze <danikaze@gmail.com>
2018-12-17 22:47:47 +0900

    Use sqlite schema in admin: upload new photos (removing all legacy settings model)

--- backend/controllers/newphoto.js
+++ backend/controllers/newphoto.js
@@ -1,26 +1,16 @@
 const multer = require('multer');
 const path = require('path');
 const auth = require('../utils/auth');
-const settingsModel = require('../models/settings');
 const stripExtension = require('../utils/stripExtension');
 const addPhoto = require('../models/gallery/add-photo');
+const getConfig = require('../models/config/get-config').getConfig;
 
-let settings;
+let routeAdmin;
+let upload;
 
-settingsModel.on('update', updateSettings);
-updateSettings();
-const uploadStorage = multer({
-  storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', settingsModel.data.images.originalPath),
-    filename: getUploadFilename,
-  }),
-});
-const upload = uploadStorage.single('photo');
-
-function updateSettings() {
-  settings = settingsModel.data.controllers.admin;
-}
-
+/**
+ * Process the request to upload a new photo
+ */
 function newPhoto(request, response) {
   upload(request, response, (uploadError) => {
     if (uploadError) {
@@ -28,43 +18,60 @@ function newPhoto(request, response) {
     }
 
     if (!request.file) {
-      response.redirect(`${settings.route}?error=file`);
+      response.redirect(`${routeAdmin}?error=file`);
       return;
     }
 
-    const body = request.body;
-    const visible = body.visible === undefined ? !settings['images.hiddenByDefault']
-                                               : request.body.visible;
-    const tags = body.tags ? body.tags.split(',').map((tag) => tag.trim()).filter((tag) => tag.length > 0)
-                           : [];
+    getConfig().then((config) => {
+      const body = request.body;
+      const visible = body.visible === undefined ? !config['images.hiddenByDefault']
+                                                 : request.body.visible;
+      const tags = body.tags ? body.tags.split(',').map((tag) => tag.trim()).filter((tag) => tag.length > 0)
+                             : [];
 
-    const photo = {
-      original: request.file.path,
-      title: body.title,
-      slug: body.slug,
-      tags,
-      keywords: body.keywords,
-      visible,
-    };
+      const photo = {
+        original: request.file.path,
+        title: body.title,
+        slug: body.slug,
+        tags,
+        keywords: body.keywords,
+        visible,
+      };
 
-    addPhoto(photo).then(() => {
-      response.redirect(settings.route);
-    }).catch((error) => {
-      response.redirect(`${settings.route}?error=${Object.keys(error).join(',')}`);
+      addPhoto(photo).then(() => {
+        response.redirect(`${routeAdmin}?newphoto`);
+      }).catch((error) => {
+        response.redirect(`${routeAdmin}?error=${Object.keys(error).join(',')}`);
+      });
     });
   });
 }
 
+/**
+ * Resolve the filename for the original uploaded file
+ */
 function getUploadFilename(req, file, cb) {
   const filename = `${stripExtension(file.originalname)}-${new Date().getTime()}${path.extname(file.originalname)}`;
   cb(null, filename);
 }
 
-module.exports = (app) => [
-  {
-    method: 'post',
-    path: `${settings.route}/photos`,
-    callback: newPhoto,
-    middleware: auth.middleware(),
-  },
-];
+module.exports = (app, serverSettings, config) => {
+  routeAdmin = serverSettings.adminUrl;
+
+  const uploadStorage = multer({
+    storage: multer.diskStorage({
+      destination: path.resolve(__dirname, '..', config['images.originalPath']),
+      filename: getUploadFilename,
+    }),
+  });
+  upload = uploadStorage.single('photo');
+
+  return [
+    {
+      method: 'post',
+      path: `${routeAdmin}/photos`,
+      callback: newPhoto,
+      middleware: auth.middleware(),
+    },
+  ];
+};
