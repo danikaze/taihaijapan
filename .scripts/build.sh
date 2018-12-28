df2a3572f8c6c7501e8b1a42de77273ae012ce7d
danikaze <danikaze@gmail.com>
2018-12-28 15:41:15 +0900

    .scripts added

---
+++ .scripts/build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+COLOR_MESSAGE='\033[0;36m'
+COLOR_IMPORTANT='\033[1;31m'
+COLOR_NC='\033[0m'
+
+NODEBIN="./node_modules/.bin"
+TSC="$NODEBIN/tsc"
+WEBPACK="$NODEBIN/webpack"
+
+mkdir -p build
+
+trap "pkill -P $$" EXIT
+trap "exit 0" INT TERM ERR
+
+echo -e "${COLOR_MESSAGE}Building backend code:${COLOR_NC}"
+
+echo -e " * Building TypeScript files..."
+$TSC -p tsconfig.backend.json
+
+echo -e " * Copying other files..."
+cp package.json build
+rsync -a --prune-empty-dirs --exclude '*.ts' --exclude 'public' --exclude '.DS_Store' backend build
+
+echo
+echo -e "${COLOR_MESSAGE}Building frontend code:${COLOR_NC}"
+echo -e " * Running webpack..."
+$WEBPACK --config webpack.prod.config.js
+
+echo -e " * Copying other files..."
+rsync -a --prune-empty-dirs --exclude '.DS_Store' frontend/public/ build/backend/public
+
+echo
