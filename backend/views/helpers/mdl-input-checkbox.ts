de43f5c9a25eddd17f525fa04aa8908901287d15
danikaze <danikaze@gmail.com>
2019-01-22 23:35:45 +0900

    Refactor hbs checkbox+switch into styles

--- backend/views/helpers/mdl-input-checkbox.ts
+++ backend/views/helpers/mdl-input-checkbox.ts
@@ -1,21 +1,36 @@
 interface HelperParams {
   hash: {
-    id?: string;
+    id: string;
+    style?: 'checkbox' | 'switch';
     name?: string;
     checked?: boolean;
     label?: string;
   };
 }
 
+const labelClasses = {
+  checkbox: 'mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect',
+  switch: 'hide-button mdl-switch mdl-js-switch mdl-js-ripple-effect',
+};
+const inputClasses = {
+  checkbox: 'mdl-checkbox__input',
+  switch: 'mdl-switch__input',
+}
+const spanClasses = {
+  checkbox: 'mdl-checkbox__label',
+  switch: 'switch-text',
+};
+
 function mdlInputCheckbox({ hash }: HelperParams): string {
-  return `<label class="mdl-checkbox mdl-js-checkbox mdl-js-ripple-effect" for="${hash.id}">
+  const style = hash.style || 'checkbox';
+  return `<label class="${labelClasses[style]}" for="${hash.id}">
   <input type="checkbox"
-         class="mdl-checkbox__input"
          value="true"
-         ${hash.id && `id="${hash.id}"`}
+         class="${inputClasses[style]}"
+         id="${hash.id}"
          ${hash.name && `name="${hash.name}"`}
          ${hash.checked && 'checked'}>
-  <span class="mdl-checkbox__label">${hash.label}</span>
+  <span class="${spanClasses[style]}">${hash.label}</span>
 </label>`;
 }
 
