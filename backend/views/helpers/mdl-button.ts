326fb32412de9a6d446c129bd87e4ed132f562ee
danikaze <danikaze@gmail.com>
2019-01-22 23:19:13 +0900

    Refactor hbs-button to accept different styles

--- backend/views/helpers/mdl-button.ts
+++ backend/views/helpers/mdl-button.ts
@@ -1,12 +1,19 @@
 interface HelperParams {
   hash: {
+    style: 'raised' | 'action' | 'accent'
     id: string;
     label?: string;
   };
 }
 
+const styleClasses = {
+  raised: 'mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect',
+  action: 'mdl-button',
+  accent: 'mdl-button mdl-button--accent mdl-button-colored',
+};
+
 function mdlButton({ hash }: HelperParams): string {
-  return `<button ${hash.id && `id="${hash.id}"`} class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect">
+  return `<button ${hash.id && `id="${hash.id}"`} class="${styleClasses[hash.style]}">
   ${hash.label}
 </button>`;
 }
